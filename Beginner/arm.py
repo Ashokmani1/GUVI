4ba3ffe51ec0f938475eaf7e5d042fcb71a66ff9
Ashokmani1 <46559785+Ashokmani1@users.noreply.github.com>
2019-06-11 20:10:51 +0530

    Create arm.py

---
+++ Beginner/arm.py
@@ -0,0 +1,14 @@
+def arm(s):
+  s=list(s)
+  x=0
+  for i in range(len(s)):
+    x=x+s[i]**3
+  if x == s:
+    return True
+  else:
+    return False
+ a=int(input())
+ if arm(a):
+  print("yes")
+else:
+  print("no")
