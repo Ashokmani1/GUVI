c31771169017f2d3cdd916041a19231d3c83a78b
Ashokmani1 <46559785+Ashokmani1@users.noreply.github.com>
2019-06-11 20:02:13 +0530

    Create b_prime.py

---
+++ Beginner/b_prime.py
@@ -0,0 +1,18 @@
+import math
+def prime(a1):
+  x=0
+  if a1 == 2 and a1 == 3:
+    return True
+  for i in range(2,int(math.sqrt(a1))+1):
+    if a1%i==0:
+      return False
+      x=x+1
+      break
+  if x==0:
+    return True
+  
+a1=int(input())
+a2=int(input())
+for i in range(a1+1,a2):
+  if prime(i):
+    print(i)
