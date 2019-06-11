686e03060b74ef85938236c9af0b5b9e7c441af9
Ashokmani1 <46559785+Ashokmani1@users.noreply.github.com>
2019-06-11 19:40:04 +0530

    Update prime.py

--- Beginner/prime.py
+++ Beginner/prime.py
@@ -3,9 +3,10 @@ a1=int(input())
 x=0
 if a1 == 2 and a1 == 3:
   print("yes")
-for i in range(2,int(math.sqrt(a1))):
+for i in range(2,int(math.sqrt(a1))+1):
   if a1%i==0:
     print("no")
     x=x+1
+    break
 if x==0:
   print("yes")
