9a0866c070cff59e4300316f4a365aa34c439a76
Ashokmani1 <46559785+Ashokmani1@users.noreply.github.com>
2019-06-12 14:56:57 +0530

    Update a.py

--- set10/a.py
+++ set10/a.py
@@ -1,1 +1,14 @@
-a=input()
+def sumOfAP( a, d,n) : 
+    sum = 0
+    i = 0
+    while i < n : 
+        sum = sum + a 
+        a = a + d 
+        i = i + 1
+    return sum
+      
+a2=list(map(int,input().split()))
+n = a2[2]
+a = a2[0]
+d = a2[1]
+print (sumOfAP(a, d, n)) 
