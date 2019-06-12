7e909b2b4eab5b9c3aaaa2e1a1fb702311275de8
Ashokmani1 <46559785+Ashokmani1@users.noreply.github.com>
2019-06-12 12:02:11 +0530

    Update change_middle.py

--- set8/change_middle.py
+++ set8/change_middle.py
@@ -2,4 +2,7 @@ a1=list(input())
 if len(a1)%2==0:
     a1[int(len(a1)/2)] ='*'
     a1[int(len(a1)/2)-1]='*'
-print(a1)
+else:
+    a1[int(len(a1)/2)] ='*'
+for i in range(0,len(a1)):
+    print(a1[i],end='')
