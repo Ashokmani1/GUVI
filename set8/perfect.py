3a2124fc885bb8fa13d06300e5accc15d822d1e1
Ashokmani1 <46559785+Ashokmani1@users.noreply.github.com>
2019-06-12 12:19:41 +0530

    Update and rename a.py - Copy.py to perfect.py

---
+++ set8/perfect.py
@@ -0,0 +1,12 @@
+a1=list(map(int,input().split()))
+i=1
+x=0
+while True:
+   if (a1[1] * a1[0])  == (i*i):
+       print("yes")
+       x=x+1
+       break
+   if (a1[1] * a1[0]) < (i*i):
+       print("no")
+       break
+   i=i+1
