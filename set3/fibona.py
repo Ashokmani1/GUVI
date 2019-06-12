22881b475de0dd30613215a239e1e19815ff93d6
Ashokmani1 <46559785+Ashokmani1@users.noreply.github.com>
2019-06-12 06:56:23 +0530

    Update fibona.py

--- set3/fibona.py
+++ set3/fibona.py
@@ -1,11 +1,11 @@
-a1=[]
-a1.append(1)
-a1.append(1)
-a2=int(input())
-if a2 <3:
-  print(a1[0])
+a3=[]
+a3.append(1)
+a3.append(1)
+a4=int(input())
+if a3 <3:
+  print(a3[0])
 else:
-  for i in range(2,a2):
-    a1.append(a1[i-1] + a1[i-2])
-for i in range(0,len(a1)):
-  print(a1[i],end=' ')
+  for i in range(2,a4):
+    a3.append(a3[i-1] + a3[i-2])
+for i in range(0,len(a3)):
+  print(a3[i],end=' ')
