646c09fbe9f084e7c19bdc5db963d8a74e72738b
Ashokmani1 <46559785+Ashokmani1@users.noreply.github.com>
2019-06-12 09:47:54 +0530

    Update and rename a_p - Copy (7).py to digit_chr.py

---
+++ set6/digit_chr.py
@@ -0,0 +1,15 @@
+a1=input()
+x=0
+for i in range(0,len(a1)):
+  if a1[i].isdigit():
+    x=x+1
+    #print(x)
+    break
+for i in range(0,len(a1)):
+  if a1[i].isalpha():
+    x=x+1
+    break
+if x==2:
+  print("yes")
+else:
+  print("no")
