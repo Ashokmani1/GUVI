08307eb430ac6f15fbc780f951c190dc084831b8
Ashokmani1 <46559785+Ashokmani1@users.noreply.github.com>
2019-06-13 06:30:09 +0530

    Update remove_vowels.py

--- Player/set2/remove_vowels.py
+++ Player/set2/remove_vowels.py
@@ -1,6 +1,6 @@
 a2= int(input())
-li=['a','e','i','o','u']
+li=['a','e','i','o','u','A','E','I','O','U']
 name1 = input()
 for i in range(0,len(li)):
     name1 = name1.replace(li[i],'')
-print(name1)
+print(name1[::-1])
