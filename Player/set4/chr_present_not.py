e63227966d3f971d18241b069f1d0642b0ac3cfa
Ashokmani1 <46559785+Ashokmani1@users.noreply.github.com>
2019-06-13 11:54:20 +0530

    Update chr_present_not.py

--- Player/set4/chr_present_not.py
+++ Player/set4/chr_present_not.py
@@ -1,10 +1,7 @@
-a1 =  input().split()
-a2 =  input().split()
-if a2.count(a1[1]) > 0:
+a3 =  input().split()
+a4 =  input().split()
+if a4.count(a3[1]) > 0:
     print("Yes")
 else:
     print("No")
-Input : 5 2
-1 2 3 4 5
-Output : 
-Yes
+
