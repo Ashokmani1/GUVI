8a7a4a43a6f9128e8a2fe5a3e7092028bcbd495c
Ashokmani1 <46559785+Ashokmani1@users.noreply.github.com>
2019-06-13 10:32:54 +0530

    Update max_element.py

--- Player/set3/max_element.py
+++ Player/set3/max_element.py
@@ -1,6 +1,10 @@
 a1 = list(map(int,input().split()))
-a2 = list(map(int,input().split()))
-a3 = list(map(int,input().split()))
+while True:
+    x=input().split()
+    if len(x) != 0:
+        a2 = list(map(int,x))
+        a3 = list(map(int,input().split()))
+        break
 for i in a3:
     a2.append(i)
     print(max(a2),end=' ')
