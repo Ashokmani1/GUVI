5a052da3da1b99863f666e78bd98212075db5124
Ashokmani1 <46559785+Ashokmani1@users.noreply.github.com>
2019-06-14 09:29:51 +0530

    Update and rename a - Copy (3).py to comon_num.py

---
+++ Player/set7/comon_num.py
@@ -0,0 +1,10 @@
+a1  = int(input())
+a2 = input().split()
+a3 = input().split()
+for i in a2:
+    if a3.count(i) > 0 :
+        print(i,end=' ')
+        x = a3.index(i)
+        a3.pop(x)
+        
+    
