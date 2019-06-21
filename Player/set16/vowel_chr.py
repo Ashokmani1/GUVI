0dfe720e92814b44daea9106fb28e1ad93c9a50a
Ashokmani1 <46559785+Ashokmani1@users.noreply.github.com>
2019-06-21 20:40:03 +0530

    Update and rename a - Copy (3).py to vowel_chr.py

---
+++ Player/set16/vowel_chr.py
@@ -0,0 +1,7 @@
+a1 = input()
+a2 = ['a','e','i','o','u']
+x=0
+for i in range(0,len(a1)-1):
+        if (a1[i] not in a2 and a2.count(a1[i+1]) == 1) or (a1[i+1] not in a2  and a2.count(a1[i]) == 1):
+            x=x+1
+print(x+1)
