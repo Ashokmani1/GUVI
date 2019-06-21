cf64ca24de84eaaac308922099511330da5a724c
Ashokmani1 <46559785+Ashokmani1@users.noreply.github.com>
2019-06-21 19:49:30 +0530

    Update reverse_weigth.py

--- Player/set15/reverse_weigth.py
+++ Player/set15/reverse_weigth.py
@@ -2,6 +2,6 @@ a3 = int(input())
 a1 = list(map(int , input().split()))
 a2 = list(map(int , input().split()))
 a3 = a2[:]
-a3.sort(reverse = True)
+a3.sort()
 for i in a3:
     print(a1[a2.index(i)],end=' ')
