b39e629e64a360bf32b1937485fb65cb331c1a4c
Ashokmani1 <46559785+Ashokmani1@users.noreply.github.com>
2019-06-21 19:48:06 +0530

    Update and rename a - Copy (4).py to reverse_weigth.py

---
+++ Player/set15/reverse_weigth.py
@@ -0,0 +1,7 @@
+a3 = int(input())
+a1 = list(map(int , input().split()))
+a2 = list(map(int , input().split()))
+a3 = a2[:]
+a3.sort(reverse = True)
+for i in a3:
+    print(a1[a2.index(i)],end=' ')
