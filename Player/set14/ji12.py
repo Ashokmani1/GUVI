f6a6b2001acdf6a3f1d543be835eaba2a844ad99
Ashokmani1 <46559785+Ashokmani1@users.noreply.github.com>
2019-07-12 20:42:44 +0530

    Update ji12.py

--- Player/set14/ji12.py
+++ Player/set14/ji12.py
@@ -1,16 +1,10 @@
-num1=input()
-arr1=[]
-count=0
-for i in range(0,len(num1)-1):
-    sum=int(num1[i])+int(num1[i+1])
-    if sum%2!=0:
-        count+=1
-    else:
-        arr1.append(count)
-        count=0
-    arr1.append(count)
-m=max(arr1)
-if m==0:
-    print(0)
-else:
-    print(m+1)
+import sys
+n1,k=map(int,input().split())
+l=[]
+for i in range(0,n1):
+    l.append(list(map(int,input().split())))
+for i in range(0,n1):
+    if l[i][1]==k:
+        print("yes")
+        sys.exit()
+print("no")
