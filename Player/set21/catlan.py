70c50dd097a74b62c9ce181c2ae51f2f6da879d1
Ashokmani1 <46559785+Ashokmani1@users.noreply.github.com>
2019-07-12 05:51:18 +0530

    Update and rename a - Copy (2).py to catlan.py

---
+++ Player/set21/catlan.py
@@ -0,0 +1,15 @@
+def catalan(nnn):
+    if nnn<=1:
+        return 1
+    else:
+        sss=0
+        for i in range(nnn):
+            sss=sss+catalan(i)*catalan(nnn-i-1)
+        return sss
+mm=int(input())
+if mm==0:
+    mm=1
+c=[]
+for i in range(mmm):
+    c.append(catalan(i))
+print(*c)
