9852d9851a57849167e2ad24fcb1e1028d01aaa0
Ashokmani1 <46559785+Ashokmani1@users.noreply.github.com>
2019-07-13 17:40:49 +0530

    Update and rename a - Copy (4).py to sam1.py

---
+++ Player/set21/sam1.py
@@ -0,0 +1,13 @@
+num1=int(input())
+arr1=list(map(int,input().split()))
+sum=0
+for i in arr1:
+    if i<0:
+        sum=sum+i
+print(sum)num1=int(input())
+arr1=list(map(int,input().split()))
+sum=0
+for i in arr1:
+    if i<0:
+        sum=sum+i
+print(sum)
