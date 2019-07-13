c5fba4e487a3d76547b26e3e67cc25582d9a0f17
Ashokmani1 <46559785+Ashokmani1@users.noreply.github.com>
2019-07-13 17:32:44 +0530

    Create sy1.py

---
+++ Player/set19/sy1.py
@@ -0,0 +1,14 @@
+n1=int(input())
+v=['a','e','i','o','u','A','E','I','O','U']
+cnt=0
+a=[]
+for i in range(n1):
+    s1=input()
+    for i in s1:
+        if i in v:
+            cnt+=1 
+    a.append([s,cnt])
+    cnt=0
+a.sort(key=lambda x:x[1],reverse=True)
+for i in range(n1):
+    print(a[i][0])
