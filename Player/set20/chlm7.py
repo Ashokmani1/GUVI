77bcf1dff2236e1eac7553be1d6b46b27fa22a7f
Ashokmani1 <46559785+Ashokmani1@users.noreply.github.com>
2019-07-13 16:27:29 +0530

    Update chlm7.py

--- Player/set20/chlm7.py
+++ Player/set20/chlm7.py
@@ -1,10 +1,10 @@
 s1=str(input())
 n=len(s1)
-c=0
+c1=0
 for i in range(0,n//2):
-    if s1[i]!=s[n-i-1]:
-        c=c+1
-if c<=1:
+    if s1[i]!=s1[n-i-1]:
+        c1=c1+1
+if c1<=1:
     print("yes")
 else:
     print("no")
