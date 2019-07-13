a1cb0ed6083f2c213279bcd0392f29ffb51dbafc
Ashokmani1 <46559785+Ashokmani1@users.noreply.github.com>
2019-07-13 11:49:14 +0530

    Update a - Copy (4).py

--- Player/set11/a - Copy (4).py
+++ Player/set11/a - Copy (4).py
@@ -1,1 +1,12 @@
-a=input()
+n1=int(input())
+l1=[int(i) for i in input().split()]
+r=[]
+for i in range(len(l1)-1):
+    if(l1[i]>l1[i+1]):
+        r.append(l1[i])
+    else:
+        r.append(l1[i+1])
+s=0
+for i in range(len(r)):
+    s=s+r[i]
+print(s)
