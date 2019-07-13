df108233f3e344ab879d79a8368c6573819a6e37
Ashokmani1 <46559785+Ashokmani1@users.noreply.github.com>
2019-07-13 15:24:12 +0530

    Create ch4.py

---
+++ Player/set17/ch4.py
@@ -0,0 +1,13 @@
+n1=list(map(str,input()))
+f1=[]
+s=0
+for i in range(len(n1)):
+    if n1[i]!=" " and n1[i] not in f1:
+        if s<n1.count(n1[i]):
+            s=n1.count(n1[i])
+            f1=[n1[i]]
+        elif s==n1.count(n1[i]):
+            f.append(n1[i])
+k="".join(f1)
+print(s,end=" ")
+print(k)
