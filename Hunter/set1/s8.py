82a6471f91c2665a440bb5074250f31c8fc64ce4
Ashokmani1 <46559785+Ashokmani1@users.noreply.github.com>
2019-07-16 07:21:08 +0530

    Update and rename a_p - Copy.py to s8.py

---
+++ Hunter/set1/s8.py
@@ -0,0 +1,7 @@
+a1=int(input())
+l=list(map(int,input().split()[:a1]))
+for i in range(len(l)):
+    for j in range(len(l)):
+        for k in range(len(l)):
+            if((l[i]+l[j]==l[k])and(i<j<k)):
+                print(l[i],l[j],l[k])
