ac0601440637e2a8026ec95998688ed2784dd8aa
Ashokmani1 <46559785+Ashokmani1@users.noreply.github.com>
2019-07-16 06:26:17 +0530

    Update s1.py

--- Hunter/set1/s1.py
+++ Hunter/set1/s1.py
@@ -1,12 +1,12 @@
 n=int(input())
-num=list(map(int,input().split()))
+num1=list(map(int,input().split()))
 l1=[]
-for i in num:
-    if num.count(i)>1:
+for i in num1:
+    if num1.count(i)>1:
         if str(i) not in l1:
             l1.append(str(i))
 if len(l1)==0:
     print("unique")
 else:
-    num.sort()
+    num1.sort()
     print(" ".join(l1))
