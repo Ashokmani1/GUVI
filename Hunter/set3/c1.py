c2c0f99e4b8d72413830069434822fd58726dbbb
Ashokmani1 <46559785+Ashokmani1@users.noreply.github.com>
2019-07-17 19:09:02 +0530

    Update and rename a_p - Copy (2).py to c1.py

---
+++ Hunter/set3/c1.py
@@ -0,0 +1,21 @@
+row1,col1 = map(int,input().split())
+b1 = []
+c1 = []
+for i in range(row1):
+	b.append(list(map(int,input().split())))
+for i in b1:
+	if 0 in i:
+		for j in range(len(i)):
+			if(i[j] == 0):
+				c1.append(j)
+			i[j] = 0
+for i in b1:
+	for j in c1:
+		i[j] = 0
+for i in b1:
+	for j in range(len(i)):
+		i[j] = str(i[j])
+for i in b1:
+	print(' '.join(i))
+  
+  
