467136a37843a04b3ab2196f08c717b9f34768c8
Ashokmani1 <46559785+Ashokmani1@users.noreply.github.com>
2019-07-19 23:33:46 +0530

    Update and rename a_p - Copy (2).py to y1.py

---
+++ Hunter/set7/y1.py
@@ -0,0 +1,10 @@
+n1=int(input())
+lo=[int(x) for x in input().split()]
+u,v=map(int,input().split())
+a=[]
+for i in range(n1-1):
+    for j in range(i+1,n1+1):
+        m=lo[i:j]
+        if (m[0]==u and m[-1]==v) or (m[-1]==u and m[0]==v):
+            a.append(len(m))
+print(min(a)-1) 
