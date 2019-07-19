726a99fc645958d769efbe7eb8369f7c598875af
Ashokmani1 <46559785+Ashokmani1@users.noreply.github.com>
2019-07-19 19:23:12 +0530

    Update and rename a_p - Copy (4).py to a5.py

---
+++ Hunter/set5/a5.py
@@ -0,0 +1,19 @@
+n1=int(input())
+
+k=format(n1+1,"b")
+
+p=[]
+
+for x in k:
+
+    if(x=='0'):
+    
+        p.append(3)
+        
+    else:
+    
+        p.append(4)
+        
+s="".join(map(str,p))
+
+print(s[1:]
