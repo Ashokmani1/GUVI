d87fca553ac62d479b41f5011afccbd2dbca5d1f
Ashokmani1 <46559785+Ashokmani1@users.noreply.github.com>
2019-07-20 22:59:21 +0530

    Update a_p - Copy (7).py

--- Hunter/set10/a_p - Copy (7).py
+++ Hunter/set10/a_p - Copy (7).py
@@ -1,1 +1,19 @@
-a=int(input())
+import sys,string
+def dgtSum(n) :
+    sum1 = 0
+    while n :
+        sum1 += n%10
+        n //= 10
+    return sum1
+
+n = int(input())
+if n <= 9:
+    print(n)
+    sys.exit()
+a1 = n // 9
+b1 = n % 9
+if b1 :
+    s = str(b1) + str('9') * a1
+else :
+    s = str('9') * a1
+print(s)
